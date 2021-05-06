170c473efee22b3956eac5b2dfb611fa40dd9728
kshitijD38 <47937542+kshitijD38@users.noreply.github.com>
2021-05-06 16:43:33 +0530

    .

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pagination_InfinityScrolling
+
 Created with CodeSandbox
+
+# for infite scrolling
+
+## client heigth, scroll height and scroll top are needed
--- src/App.js
+++ src/App.js
@@ -4,21 +4,32 @@ import "./styles.css";
 export default function App() {
   const [pageNumber, setPageNumber] = useState(1);
   const [limit] = useState(10);
-  const api = `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${pageNumber}`;
-
+  // const api = `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${pageNumber}`;
+  const api = `https://jsonplaceholder.typicode.com/todos`;
   const [dataArray, setDataArray] = useState([]);
+  // const [totalArray, setTotalArray] = useState([]);
+  const [pageLength, setPageLength] = useState(0);
+
+  function paginateApiData(page, limit, apiData1) {
+    const firstItem = page * limit - limit;
+    const lastItem = page * limit;
+    return apiData1.slice(firstItem, lastItem);
+  }
 
   useEffect(() => {
     const fetchData = async () => {
       const apiData = await fetch(api);
       const apiDataJson = await apiData.json();
-      setDataArray([...apiDataJson]);
+      setPageLength(apiDataJson.length);
+      // setTotalArray(apiDataJson);
+      setDataArray(paginateApiData(pageNumber, limit, apiDataJson));
     };
     fetchData();
-  }, [api]);
 
-  const MAX_PAGE_NUMBER = 10;
-  const pageNumberArray = new Array(MAX_PAGE_NUMBER)
+    // return () => setDataArray([]);
+  }, [pageNumber, limit, api]);
+
+  const pageNumberArray = new Array(Math.ceil(pageLength / limit))
     .fill(1)
     .map((_, index) => index + 1);
 
@@ -31,14 +42,6 @@ export default function App() {
     setPageNumber(pageNumber + 1);
   };
 
-  const apiData1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const paginateApiData = (page, limit) => {
-    const firstItem = page * limit - limit;
-    const lastItem = page * limit;
-    return apiData1.slice(firstItem, lastItem);
-  };
-  console.log(paginateApiData(1, 5));
-
   return (
     <div className="app">
       <h1 style={{ color: "black" }}>Pagination / Infinite Scrolling</h1>
@@ -50,7 +53,10 @@ export default function App() {
       <button disabled={pageNumber === 1} onClick={handlePrev}>
         Prev
       </button>
-      <button disabled={pageNumber === MAX_PAGE_NUMBER} onClick={handleNext}>
+      <button
+        disabled={pageNumber === Math.ceil(pageLength / limit)}
+        onClick={handleNext}
+      >
         Next
       </button>
       <div>
